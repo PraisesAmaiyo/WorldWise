0e1a8effafde51d5db4c9294e76cbf1ece1eae40
Amaiyo Praises <amaiyo.praises@gmail.com>
2024-02-06 21:15:53 +0100

    MOD - Updated the routing of the navigations

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import styles from './Homepage.module.css';
-import PageNav from '../components/AppNav';
+import PageNav from '../components/PageNav';
 
 export default function Homepage() {
   return (
