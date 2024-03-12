aac64d3fc1ce6a3ffa8177009b12f6d6803902d6
Amaiyo Praises <amaiyo.praises@gmail.com>
2024-03-12 14:13:31 +0100

    MOD

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
@@ -10,6 +10,7 @@ import PageNotFound from './pages/PageNotFound';
 import CityList from './components/CityList';
 import CountryList from './components/CountryList';
 import City from './components/City';
+import Form from './components/Form';
 
 const BASE_URL = 'http://localhost:9000';
 
@@ -41,10 +42,7 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
@@ -54,7 +52,7 @@ function App() {
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,8 +1,12 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useSearchParams } from 'react-router-dom';
 import styles from './City.module.css';
 
 function City() {
   const { id } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
 
   // TEMP DATA
   const currentCity = {
@@ -12,9 +16,16 @@ function City() {
     notes: 'My favorite city so far!',
   };
 
-  //   const { cityName, emoji, date, notes } = currentCity;
+  const { cityName, emoji, date, notes } = currentCity;
 
-  return <h1>City {id}</h1>;
+  return (
+    <>
+      <h1>City {id} </h1>
+      <p>
+        Position: {lat}, {lng}
+      </p>
+    </>
+  );
 
   //   return (
   //     <div className={styles.city}>
