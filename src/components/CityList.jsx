43b8975561d8e4d7b922b8ba1c62cbad93e3d97b
Amaiyo Praises <amaiyo.praises@gmail.com>
2024-03-19 15:15:58 +0100

    MODIFIED

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -2,8 +2,10 @@ import Spinner from './Spinner';
 import styles from './CityList.module.css';
 import Message from './Message';
 import CityItem from './CityItem';
+import { useCities } from '../contexts/CitiesContext';
 
-function CityList({ cities, isLoading }) {
+function CityList() {
+  const { cities, isLoading } = useCities();
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
---
+++ src/contexts/CitiesContext.jsx
@@ -0,0 +1,55 @@
+import { createContext, useState, useEffect, useContext } from 'react';
+
+const BASE_URL = 'http://localhost:9000';
+
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsloading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsloading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert('There was an error loading data...');
+      } finally {
+        setIsloading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  async function getCity(id) {
+    try {
+      setIsloading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch {
+      alert('There was an error loading data...');
+    } finally {
+      setIsloading(false);
+    }
+  }
+
+  return (
+    <CitiesContext.Provider value={{ cities, isLoading, currentCity, getCity }}>
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error('CitiesContext was used outside the CitiesProvider');
+  return context;
+}
+
+export { CitiesProvider, useCities };
